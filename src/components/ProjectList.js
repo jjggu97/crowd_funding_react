14e7d90922a9f08b29394dc7a0131397be333448
jjggu97 <jjggu@naver.com>
2024-06-19 15:35:51 +0900

    06/19

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -4,15 +4,15 @@ import '../styles/ProjectList.css';
 const ProjectList = () => {
   const projects = Array.from({ length: 18 }, (_, i) => ({
     id: i + 1,
-    title: `Project ${i + 1}`,
-    description: `Description of Project ${i + 1}`
+    title: `프로젝트 ${i + 1}`,
+    description: `설명 ${i + 1}`
   }));
 
   return (
     <div className="project-list">
       {projects.map((project) => (
         <div key={project.id} className="project-card">
-          <div className="project-image">Image</div>
+          <div className="project-image"></div>
           <h2>{project.title}</h2>
           <p>{project.description}</p>
         </div>
