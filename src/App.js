53d3ecc6b263726ae95e3c66d6b19ef6a615823b
jjggu97 <jjggu@naver.com>
2024-06-20 17:10:35 +0900

    06/20

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import IntroPage from './pages/IntroPage';
+import FundingPage from './pages/FundingPage';
 import './App.css';
 
 
@@ -16,6 +17,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<IntroPage />} />
             <Route path="/main" element={<HomePage />} />
+            <Route path="/funding" element={<FundingPage />}/>
           </Routes>
         </main>
         <Footer />
---
+++ src/pages/FundingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../styles/FundingPage.css"
+
+const FundingPage = () => {
+  return (
+    <div className="fund-page">
+
+    </div>
+  );
+};
+
+export default FundingPage;
